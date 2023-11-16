5f03906b6817d508f135c35574be58daef368662
Shrivardhan C <83063788+Gloww144@users.noreply.github.com>
2023-11-16 14:20:06 +0530

    Co-authored-by: sshebbar <sshebbar@users.noreply.github.com> Co-authored-by: The Protagonist <TheProtagonist1389@users.noreply.github.com>

--- api.py
+++ api.py
@@ -2,13 +2,12 @@ import requests
 from requests.structures import CaseInsensitiveDict
 from opencage.geocoder import OpenCageGeocode
 
-def GetParking(lat, long, rad):
-        
 
+def GetParking(lat, long, rad, disabled = False):
     key2 = '1643b4cf13a64d90b17d34a4a8f2b3ff'
     KEY = '7d054aedbf7841b4b2154afb427cfd8b'
 
-    url = f"https://api.geoapify.com/v2/places?categories=parking&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=1&apiKey={KEY}"
+    url = f"https://api.geoapify.com/v2/places?categories=parking&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
 
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
@@ -16,17 +15,18 @@ def GetParking(lat, long, rad):
     resp = requests.get(url, headers=headers)
     Response = eval(resp.text)
 
-
+    
     places = []
-
     for item in Response["features"]:
+        
         lat = item["properties"]["lat"]
         lon = item["properties"]["lon"]
         gc = OpenCageGeocode(key2)
 
 
         for place in gc.reverse_geocode(lat,lon):
-            print(place["formatted"])
             places.append({"lat":lat,"long":lon,"address":place["formatted"]})
 
-    return (lat, lon)
+    return places
+
+
--- main.py
+++ main.py
@@ -20,21 +20,35 @@ def login():
 @app.route("/spots",methods=["GET","POST"])
 def spots():
     if request.method == "POST":
-        lat = request.form.get("latitude")
-        long = request.form.get("longitude")
+        lat = float(request.form.get("latitude"))
+        long = float(request.form.get("longitude"))
         key2 = '1643b4cf13a64d90b17d34a4a8f2b3ff'
         KEY = '7d054aedbf7841b4b2154afb427cfd8b'
-        rad = 1000
-        url = f"https://api.geoapify.com/v2/places?categories=parking&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
+        rad = 750
+        print(request.form)
+        d = request.form.get("disabled")
+        if d:
+            url = f"https://api.geoapify.com/v2/places?categories=parking&conditions=wheelchair.yes&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
+        else:
+            url = f"https://api.geoapify.com/v2/places?categories=parking&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
 
+        print(rad)
         headers = CaseInsensitiveDict()
         headers["Accept"] = "application/json"
-
         resp = requests.get(url, headers=headers)
         Response = eval(resp.text)
 
 
-        places = []
+        url = f"https://api.geoapify.com/v2/places?categories=parking&conditions=wheelchair.yes&filter=circle:{long},{lat},{rad + 1000}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
+
+        url1 = f"https://api.geoapify.com/v2/places?categories=parking&filter=circle:{long},{lat},{rad}&bias=proximity:{long},{lat}&limit=10&apiKey={KEY}"
+
+        headers = CaseInsensitiveDict()
+        headers["Accept"] = "application/json"
+        resp = requests.get(url, headers=headers)
+        Response = eval(resp.text)
+        
+        d_places = []
 
         for item in Response["features"]:
             lat = item["properties"]["lat"]
@@ -43,18 +57,29 @@ def spots():
 
 
             for place in gc.reverse_geocode(lat,lon):
-                print(place["formatted"])
-                places.append({"lat":lat,"lng":long,"address":place["formatted"]})
+                d_places.append({"lat":lat,"lng":long,"address":place["formatted"],"url":f"http://maps.google.com/maps?z=12&t=m&q=loc:{lat}+{long}"})
 
-        return render_template("spots.html",locations=places)
+        places = []
+        resp1 = requests.get(url1,headers=headers)
+        Response1 = eval(resp1.text)
+
+        for item in Response1["features"]:
+            lat = item["properties"]["lat"]
+            lon = item["properties"]["lon"]
+            gc = OpenCageGeocode(key2)
+
+
+            for place in gc.reverse_geocode(lat,lon):
+                places.append({"lat":lat,"lng":long,"address":place["formatted"],"url":f"http://maps.google.com/maps?z=12&t=m&q=loc:{lat}+{long}"})
+
+
+        return render_template("spots.html",dlocations=d_places,locations = places)
     else:
         return redirect(url_for('main'))
         
-@app.route("/route")
-def route():
-    long=13.0093
-    lat=77.5741
-    return redirect(f"http://maps.google.com/maps?z=12&t=m&q=loc:{long}+{lat}")
+@app.route('/auto')
+def auto():
+    return render_template('auto.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
